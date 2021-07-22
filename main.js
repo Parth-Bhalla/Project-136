3248cb0141a206f19710f4b5eba9d6f4334f1960
Parth-Bhalla <75519009+Parth-Bhalla@users.noreply.github.com>
2021-07-22 17:06:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+objects=[];
+status="";
+video="";
+
+function preload(){
+    video=createVideo('video.mp4');
+}
+
+function setup(){
+canvas=createCanvas(480,380);
+canvas.center();
+video.hide();
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
